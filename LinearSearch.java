438556e983c26ca2c12406adc8993689cfbef70c
Rajesh Pandit <iam.panditrajesh@gmail.com>
2023-10-31 15:23:17 +0530

    linear search: output format updated

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,13 +1,20 @@
 /**
- * LinearSearch
+ * LinearSearch : Given an array and a target value. You need to write a method,
+ * if the targetvalue is present in the array or not.
+ * if present, return index of that element
+ * else, return -1
  */
 public class LinearSearch {
 
     public static void main(String[] args) {
         int[] arr = { 12, 34, 45, 11, 78, 90 };
-        int targetValue = 45;
+        int targetValue = 452;
         int index = linearSearch(arr, targetValue);
-        System.out.println("The index of the target element is: " + index);
+        if (index == -1) {
+            System.out.println("The element is not present in the array.");
+        } else {
+            System.out.println("The index of the target element is: " + index);
+        }
     }
 
     public static int linearSearch(int[] arr, int target) {
